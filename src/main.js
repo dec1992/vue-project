1cea822e1dd4701f7967ad0ff200630af83ab521
Dec Burns <dec@decs-mbp.home>
2024-03-29 00:13:49 +0000

    init routing

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { VueFire } from 'vuefire'
 import { firebaseApp } from './firebase'
+import { createMemoryHistory, createRouter } from 'vue-router'
+import HomeComponent from './components/Home'
 
+const routes = [{ path: '/', component: HomeComponent }]
 
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+})
 
 const app = createApp(App)
 
+app.use(router)
 app.use(VueFire, { firebaseApp })
-
 app.mount('#app')
