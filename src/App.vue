fa9f05f50104191f3a948c032f3e902ec2c98190
Dec Burns <dec@decs-mbp.home>
2024-03-30 15:06:37 +0000

    record audio & styling

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,12 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+header {
+  margin: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 </style>
--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
+  <main class="m-1">
     <h3>Welcome to ChatRoom {{ chatId }}</h3>
     <UserComponent>
       <template #user="{ user }">
@@ -11,11 +11,27 @@
             />
           </li>
         </ul>
-        <input v-model="newMessageText" />
+        <input class="input" v-model="newMessageText" />
+        <hr />
+        <h5>Record Audio</h5>
 
         <button
+          class="button is-danger is-fullwidth"
+          v-if="!recorder"
+          @click="record()"
+        >
+          Record
+        </button>
+        <button class="button is-danger is-fullwidth" v-else @click="stop()">
+          Stop
+        </button>
+        <hr v-if="newAudio" />
+        <h5 v-if="newAudio">Preview Audio</h5>
+        <audio v-if="newAudio" :src="newAudioUrl" controls></audio>
+        <hr />
+        <button
           :disabled="!newMessageText || loading"
-          class="button is-success"
+          class="button is-success is-fullwidth"
           type="text"
           @click="addMessage(user.uid)"
         >
@@ -39,6 +55,12 @@ import { orderBy, limitToLast } from "firebase/firestore";
 const route = useRoute();
 const newMessageText = ref("");
 const loading = ref(false);
+const newAudio = ref(null);
+const newAudioUrl = computed(() => {
+  if (!newAudio.value) return null;
+  return URL.createObjectURL(newAudio.value);
+});
+const recorder = ref(null);
 const chatId = route.params.id;
 const contactSource = computed(() =>
   query(
@@ -60,9 +82,40 @@ async function addMessage(uid) {
   this.loading = false;
   this.newMessageText = "";
 }
+
+async function record() {
+  this.newAudio = null;
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: false,
+  });
+
+  const options = { mimeType: "audio/webm" };
+  const recordedChunks = [];
+  this.recorder = new MediaRecorder(stream, options);
+  this.recorder.addEventListener("dataavailable", (e) => {
+    if (e.data.size > 0) {
+      recordedChunks.push(e.data);
+    }
+  });
+
+  this.recorder.addEventListener("stop", () => {
+    this.newAudio = new Blob(recordedChunks);
+  });
+
+  this.recorder.start();
+}
+
+async function stop() {
+  this.recorder.stop();
+  this.recorder = null;
+}
 </script>
 
 <style scoped>
+main {
+  padding: 40px;
+}
 ul {
   list-style-type: none;
   margin: 0;
@@ -74,7 +127,10 @@ ul {
   padding: 10px;
   border-radius: 0;
 }
-
+audio {
+  width: 100%;
+  margin-top: 20px;
+}
 li {
   display: flex;
 }
